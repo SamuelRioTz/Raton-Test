b66855edf5cc247f4c52eb1d30ab3bf7b69b9fc3
Samuel <samuelriotz@hotmail.com>
2019-07-23 00:43:18 -0400

    adding git ignore

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,10 @@
 module.exports = app => {
-    app.get('/', (req, res) => {
+    app.get('*', (req, res) => {
         console.log('get')
         console.log(req.body)
         res.send('get')
     })
-        .post('/', (req, res) => {
+        .post('*', (req, res) => {
             console.log('post')
             console.log(req.body)
             res.send('post')
